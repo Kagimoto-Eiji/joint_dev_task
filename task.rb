75cc126585745c875041aa0d3df81573ae5dacf4
kagimotoeiji <thank.eiji1017@gmail.com>
2021-03-13 20:36:58 +0900

    q14~16

--- task.rb
+++ task.rb
@@ -98,8 +98,7 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!
-  sports.uniq!
+  sports.flatten!.uniq!
   sports.each.with_index(1) do |sport, i|
     puts "No#{i} #{sport}"
   end
@@ -127,7 +126,8 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-  
+  array = data.keys
+  puts array
 end
 
 def q15
@@ -135,7 +135,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-    
+  puts data1.include?(:age)? "OK" : "NG"
+  puts data2.include?(:age)? "OK" : "NG"
 end
 
 def q16
@@ -147,7 +148,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
